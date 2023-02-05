46f913ad6e378d3d0c4fbd7a8c254db3963b13f3
Imi <ekafproject@gmail.com>
2023-02-05 22:59:35 +0100

    reordering used and non-used part of the code (no changes in code)

--- Final_my-todo-app.py
+++ Final_my-todo-app.py
@@ -2,7 +2,7 @@ import functions
 import PySimpleGUI as sg
 import time
 
-sg.theme("Black")
+sg.theme("DarkTeal9")
 
 clock = sg.Text('', key='clock')
 label = sg.Text("Type in a to-do")
@@ -21,6 +21,47 @@ window = sg.Window('My To-Do-App',
                            [list_box, edit_button, complete_button],
                            [exit_button]],
                    font=('Helvetica', 20))
+
+while True:
+    event, values = window.read()
+    print(1, event)
+    print(2, values)
+    print(3, values['todos'])
+
+    if event == "Add":
+        todos = functions.get_todos()
+        new_todo = values['todo'] + "\n"
+        todos.append(new_todo)
+        functions.write_todos(todos)
+        window['todos'].update(values=todos)
+    elif event == "Edit":
+        try:
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        except IndexError:
+            print("Select an item first")
+    elif event == "Complete":
+        todo_to_complete = values['todos'][0]
+        todos = functions.get_todos()
+        todos.remove(todo_to_complete)
+        functions.write_todos(todos)
+        window['todos'].update(values=todos)
+        window['todo'].update(value='')
+    elif event == "Exit":
+        break
+    elif event == "todos":
+        window['todo'].update(value=values['todos'][0])
+    elif event == sg.WIN_CLOSED:
+        break
+
+window.close()
+
 # while True:
 #     event, values = window.read()
 #     print(1, event)
@@ -63,43 +104,3 @@ window = sg.Window('My To-Do-App',
 #
 #         case sg.WIN_CLOSED:
 #             break
-
-while True:
-    event, values = window.read()
-    print(1, event)
-    print(2, values)
-    print(3, values['todos'])
-
-    if event == "Add":
-        todos = functions.get_todos()
-        new_todo = values['todo'] + "\n"
-        todos.append(new_todo)
-        functions.write_todos(todos)
-        window['todos'].update(values=todos)
-    elif event == "Edit":
-        try:
-            todo_to_edit = values['todos'][0]
-            new_todo = values['todo']
-
-            todos = functions.get_todos()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)
-        except IndexError:
-            print("Select an item first")
-    elif event == "Complete":
-        todo_to_complete = values['todos'][0]
-        todos = functions.get_todos()
-        todos.remove(todo_to_complete)
-        functions.write_todos(todos)
-        window['todos'].update(values=todos)
-        window['todo'].update(value='')
-    elif event == "Exit":
-        break
-    elif event == "todos":
-        window['todo'].update(value=values['todos'][0])
-    elif event == sg.WIN_CLOSED:
-        break
-
-window.close()
